7df1d28bef7e663154b6aa907765fb256276e57b
Eric Echemane <eechemane29@gmail.com>
2022-09-14 20:43:13 +0800

    [refactor] remove barangay provider wrapper in _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
-import { useBarangayContext } from "providers/barangay";
 
 export default function Home() {
-  const { state } = useBarangayContext();
-  console.log(state);
 
   return (
     <div>Home</div>
