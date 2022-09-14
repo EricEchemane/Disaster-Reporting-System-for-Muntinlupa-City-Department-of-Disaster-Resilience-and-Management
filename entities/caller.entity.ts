36b760aefeca23bbafe559a1762e7285cc813da5
Eric Echemane <eechemane29@gmail.com>
2022-09-14 18:40:35 +0800

    caller schema created

---
+++ entities/caller.entity.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const callerSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "Full naem is required"],
+        minLength: [5, "Full name must be at least 3 characters long"],
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, "Phone number is required"],
+        validate: {
+            validator: (phone: string) => {
+                if (phone.startsWith('+63') === false) return false;
+                if (phone.length !== 13) return false;
+                return true;
+            },
+            message: "Invalid phone number",
+        }
+    },
+    faceImageUrl: {
+        type: String,
+        required: [true, "Face image url is required"],
+        validate: {
+            validator: (url: string) => url.startsWith('https://'),
+            message: "Invalid image url",
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+callerSchema.pre('save', function (next) {
+    this.updatedAt = new Date();
+    next();
+});
+
+export type ICaller = mongoose.InferSchemaType<typeof callerSchema>;
+
+const Caller = mongoose.model('Caller', callerSchema);
+
+export default Caller;
