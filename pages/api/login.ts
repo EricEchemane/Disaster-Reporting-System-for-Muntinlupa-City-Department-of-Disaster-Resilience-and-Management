a3a44e92f72122cf1343edbf72173351ebbc2d75
Eric Echemane <eechemane29@gmail.com>
2022-09-15 01:39:03 +0800

    [refactor]

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -16,17 +16,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (!barangay) throw new RequestError(404, 'Barangay not found');
         if (barangay.password !== password) throw new RequestError(401, 'Incorrect password');
 
-        setCookie(COOKIE_KEY, JSON.stringify(barangay), { req, res, maxAge: 60 * 60 * 24 });
-        res.status(200).json({ success: true, data: barangay });
+        setCookie(
+            COOKIE_KEY,
+            JSON.stringify(barangay),
+            { req, res, maxAge: 60 * 60 * 24 }
+        );
+        res.status(200).json({
+            success: true,
+            data: barangay,
+            userType: 'brgy'
+        });
     }
     else if (userType === 'admin') {
         if (!username) throw new RequestError(400, 'Username is missing');
         const _username = process.env.ADMIN_USERNAME;
         const _password = process.env.ADMIN_PASSWORD;
-        if (_username !== username || _password !== password) throw new RequestError(401, 'Incorrect username or password');
+        if (_username !== username || _password !== password)
+            throw new RequestError(401, 'Incorrect username or password');
 
-        setCookie(COOKIE_KEY, JSON.stringify({ type: 'admin' }), { req, res, maxAge: 60 * 60 * 24 });
-        res.status(200).json({ success: true, data: { type: 'admin' } });
+        setCookie(
+            COOKIE_KEY,
+            JSON.stringify({ type: 'admin' }),
+            { req, res, maxAge: 60 * 60 * 24 }
+        );
+        res.status(200).json({
+            success: true,
+            data: { userType: 'admin' }
+        });
     }
     else res.status(400).json({ success: false, message: 'Invalid user type' });
 }
