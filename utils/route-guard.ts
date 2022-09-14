3cc2d1376581c9d7bdc880182d0f49cde5840060
Eric Echemane <eechemane29@gmail.com>
2022-09-14 21:18:52 +0800

    ICookie interface

--- utils/route-guard.ts
+++ utils/route-guard.ts
@@ -2,11 +2,16 @@ import { GetServerSidePropsContext, PreviewData } from "next";
 import { useCookie } from "next-cookie";
 import { ParsedUrlQuery } from "querystring";
 
+interface ICookie {
+    type: "brgy" | "admin";
+    _id: string;
+}
+
 export default function routeGuard() {
     return async (context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
         // eslint-disable-next-line react-hooks/rules-of-hooks
         const cookie = useCookie(context);
-        const loginCredentials = cookie.get('disaster');
+        const loginCredentials: ICookie = cookie.get('disaster');
 
         if (!loginCredentials) {
             return {
