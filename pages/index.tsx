ceb87d181608da2f6d3b83956aeb9b535ade0e12
Eric Echemane <eechemane29@gmail.com>
2022-10-10 22:02:39 +0800

    initial landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
+import { Button, Container, CopyButton, Group, Text, Title } from "@mantine/core";
+import { IconArrowRight, IconPlayerPlay } from "@tabler/icons";
+import Head from "next/head";
+import Image from "next/image";
+import logo from "../public/muntinlupa-logo.png";
+import heroImage from "../public/assets/hero-image.jpg";
+
 export default function HomePage() {
-  // const { error, user } = useUser({
-  //   whenNotFoundRedirectTo: '/sign-in'
-  // });
 
-  // if (error) return <div>An Error occured in the request</div>;
-  // if (!user) return <div>Loading...</div>;
+  return <>
+    <Head>
+      <title> Disaster Resilience Department of Muntinlupa City </title>
+    </Head>
 
-  return (
-    <div>
-      <h1>Home Page</h1>
-      {/* <p>Welcome, {user.name}</p> */}
-    </div>
-  );
+    <Container pb={'xl'}>
+      <Group position={'apart'} my={'md'}>
+        <Group>
+          <Image width={50} height={50} alt={'muntinlupa logo'} src={logo} />
+          <Title order={4}>
+            Disaster Resilience Department
+          </Title>
+        </Group>
+        <Button color={'red'} rightIcon={<IconArrowRight />} variant={'filled'}>
+          Report Incident
+        </Button>
+      </Group>
+
+      <Group my={'md'} position={'apart'}>
+        <Group>
+          <Text size={'xl'}> Emergency Hotline: </Text>
+          <CopyButton value={'137-175'} timeout={3000}>
+            {({ copied, copy }) => (
+              <Button radius={'xl'} color={copied ? 'teal' : 'green'} onClick={copy}>
+                {copied ? '137-175 copied' : '137-175'}
+              </Button>
+            )}
+          </CopyButton>
+        </Group>
+        <Button rightIcon={<IconPlayerPlay />} variant={'light'}>
+          Video Tutorial: How to submit a report?
+        </Button>
+      </Group>
+
+      <Image alt="hero image" src={heroImage} style={{ borderRadius: "1rem" }} />
+    </Container>
+  </>;
 }
