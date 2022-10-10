5068469ef95b680515900840dc4343210e45825a
Eric Echemane <eechemane29@gmail.com>
2022-10-10 23:25:23 +0800

    added barangay list

---
+++ app/lib/barangays.ts
@@ -0,0 +1,13 @@
+const barangays: string[] = [
+    'Tunasan',
+    'Bayanan',
+    'Buli',
+    'Cupang',
+    'Poblacion',
+    'Ayala Alabang',
+    'Putatan',
+    'Sucat',
+    'Alabang',
+];
+
+export default Object.freeze(barangays);
