8369157386c1e093b82668f2dd136705f714d7d2
Eric Echemane <eechemane29@gmail.com>
2022-10-10 22:14:44 +0800

    added title

--- pages/how-to-submit-report.tsx
+++ pages/how-to-submit-report.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import React from 'react';
 
 export default function HomeToSubmitReportPage() {
-    return (
+    return <>
+        <Head>
+            <title> How to Submit A Report </title>
+        </Head>
         <div>
             <h1> Dito dapat magpeplay yung video Tutorial </h1>
         </div>
-    );
+    </>;
 }
