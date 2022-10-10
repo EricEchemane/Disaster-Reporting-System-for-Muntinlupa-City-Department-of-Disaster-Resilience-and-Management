a93da798417f68510791556e5df021aec846e314
Eric Echemane <eechemane29@gmail.com>
2022-10-10 22:15:35 +0800

    added title to reporting page

--- pages/report-incident.tsx
+++ pages/report-incident.tsx
@@ -1,7 +1,12 @@
+import Head from 'next/head';
 import React from 'react';
 
 export default function ReportIncidentPage() {
-    return (
-        <div>ReportIncidentPage</div>
-    );
+    return <>
+        <Head>
+            <title> Report Incident </title>
+        </Head>
+
+        <h1> Reporting incident page </h1>
+    </>;
 }
