d06e19008a536607a4e399769eccc41383ec20b7
Eric Echemane <eechemane29@gmail.com>
2022-10-11 17:23:26 +0800

    added initial form values

--- pages/report-incident.tsx
+++ pages/report-incident.tsx
@@ -1,12 +1,40 @@
+import { Container, Group, Title } from '@mantine/core';
 import Head from 'next/head';
-import React from 'react';
+import muntiLogo from "../public/muntinlupa-logo.png";
+import ddrmLogo from "../public/ddrm.png";
+import Image from 'next/image';
+import { useForm } from '@mantine/form';
 
 export default function ReportIncidentPage() {
+    const form = useForm({
+        initialValues: {
+            brgy: '',
+            fullName: '',
+            address: '',
+            mobileNumber: '',
+            incidentPhoto: '',
+            reportBody: '',
+            date: '',
+        }
+    });
+
     return <>
         <Head>
             <title> Report Incident </title>
         </Head>
 
-        <h1> Reporting incident page </h1>
+        <Container py={'xl'} size={'md'}>
+            <Group position={'center'}>
+                <Image width={100} height={100} alt="muntinlupa logo" src={muntiLogo} />
+                <Image width={100} height={100} alt="ddrm logo" src={ddrmLogo} />
+            </Group>
+
+            <Title order={2} align={'center'} mt={'md'}>
+                Department of Disaster Resilience and Management
+            </Title>
+            <Title order={3} align={'center'} mt={'sm'} color={'blue'}>
+                Report Incident
+            </Title>
+        </Container>
     </>;
 }
